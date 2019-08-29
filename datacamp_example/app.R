7b4c12eb47676a1b8588d96349efee1938afa79d
kazyamamoto2 <kazyamamoto2@gmail.com>
2019-08-29 14:43:53 +0900

    ui done

--- datacamp_example/app.R
+++ datacamp_example/app.R
@@ -6,20 +6,23 @@ library(tidyverse)
 ui <- fluidPage(
         sidebarLayout(
                 sidebarPanel(
-                        fileInput(inputId = "data_file", "Choose CSV File",
-                                  accept = ".csv"),
+                        fileInput(inputId = "data_file", label = "Choose CSV File",
+                                  accept = ".csv", buttonLabel = "Browse...",
+                                  placeholder = "No file selected"),
                         dateRangeInput(inputId = "date_range", label = "Period", start = NULL, end = NULL, min = NULL,
                                        max = NULL, format = "yyyy-mm-dd", startview = "day", weekstart = 0,
                                        language = "en", separator = " to ", width = NULL),
                         checkboxGroupInput(inputId = "country", label = "choose country",choices = c("Japan", "Republic of Korea","United States of America", "France"))
                         
                 ),
-                mainPanel()
+                mainPanel(plotOutput(outputId = "plot"))
         )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {}
+server <- function(input, output) {
+        "hello"
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
