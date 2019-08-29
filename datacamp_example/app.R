18386304e1c976c05a255c129fb8c1387fef70f2
kazyamamoto2 <kazyamamoto2@gmail.com>
2019-08-29 14:20:12 +0900

    shiny development until sidebar portion.

--- datacamp_example/app.R
+++ datacamp_example/app.R
@@ -1,17 +1,21 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   tags$h1("Hello World")
+        sidebarLayout(
+                sidebarPanel(
+                        fileInput(inputId = "data_file", "Choose CSV File",
+                                  accept = ".csv"),
+                        dateRangeInput(inputId = "date_range", label = "Period", start = NULL, end = NULL, min = NULL,
+                                       max = NULL, format = "yyyy-mm-dd", startview = "day", weekstart = 0,
+                                       language = "en", separator = " to ", width = NULL),
+                        checkboxGroupInput(inputId = "country", label = "choose country",choices = c("Japan", "Republic of Korea","United States of America", "France"))
+                        
+                ),
+                mainPanel()
+        )
 )
 
 # Define server logic required to draw a histogram
@@ -20,3 +24,40 @@ server <- function(input, output) {}
 # Run the application 
 shinyApp(ui = ui, server = server)
 
+
+
+df <- read_csv("/Users/kazumichiyamamoto/Desktop/who_suicide_statistics.csv")
+
+# rate of suicide
+df <- df %>%
+        mutate(percentage = suicides_no/population)
+
+# reorder the level
+df$age <- factor(df$age, levels = c("5-14 years", "15-24 years", "25-34 years", "35-54 years", "55-74 years", "75+ years"))
+
+# filter and group 4 countries
+df_filtered <- df %>%
+        filter(country %in% c("Japan", "Republic of Korea","United States of America", "France")) %>%
+        group_by(country, year) %>%
+        summarize(total_no = sum(suicides_no),
+                  total_pop = sum(population),
+                  no_per_100000 = total_no/total_pop*100000)
+
+# yearly change among countries
+ggplot(df_filtered, aes(x=year, y=no_per_100000, color = country))+
+        geom_point()+
+        geom_line()
+
+# filter korea
+df_korea <- df %>% 
+        filter(country == "Republic of Korea")
+
+# group by year and age
+df_korea_grouped <- df_korea %>%
+        group_by(year, age) %>%
+        summarize(total_no = sum(suicides_no),
+                  total_pop = sum(population),
+        )
+#graph geom_col
+ggplot(df_korea_grouped, aes(x=year, y=total_no, fill=age))+
+        geom_col()
