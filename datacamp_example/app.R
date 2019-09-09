7458f7397cf22c8edd8c3ab717fd71850e9ca827
kazyamamoto2 <kazyamamoto2@gmail.com>
2019-09-09 14:36:44 +0900

    who_suicide visualisation software

--- datacamp_example/app.R
+++ datacamp_example/app.R
@@ -20,7 +20,28 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-        "hello"
+        #uploaded data
+        csv_file <- reactive({df <- read_csv(input$data_file$datapath)
+        #mutate the rate of suicide
+        df <- df %>%
+                mutate(percentage = suicides_no/population)
+        # reorder the level
+        df$age <- factor(df$age, levels = c("5-14 years", "15-24 years", "25-34 years", "35-54 years", "55-74 years", "75+ years"))
+        # filter and group 4 countries
+        df_filtered <- df %>%
+                filter(country == input$country ) %>%
+                group_by(country, year) %>%
+                summarize(total_no = sum(suicides_no),
+                          total_pop = sum(population),
+                          no_per_100000 = total_no/total_pop*100000)
+       })
+        
+        output$plot <- renderPlot({
+                # yearly change among countries
+                ggplot(csv_file(), aes(x=year, y=no_per_100000, color = country))+
+                        geom_point()+
+                        geom_line()
+                       })
 }
 
 # Run the application 
